319f6bfc3e5ee833ea16c532c3b1f55f2c89ecd3
Aragorn <aragorn@gmail.com>
2022-09-01 02:03:39 +0800

    temp check in

--- app/models/GpsCollector.scala
+++ app/models/GpsCollector.scala
@@ -13,11 +13,11 @@ import play.api.libs.json.{JsError, Json}
 
 import java.io.BufferedReader
 
-case class GpsParam(lat:Option[Double], lon:Option[Double], radius:Option[Double], enableAlert:Option[Boolean])
-object GpsCollector extends DriverOps {
-  var count = 0
+case class GpsParam(lat: Option[Double], lon: Option[Double], radius: Option[Double], enableAlert: Option[Boolean])
 
+object GpsCollector extends DriverOps {
   val monitorTypes = List(MonitorType.LAT, MonitorType.LNG, MonitorType.ALTITUDE, MonitorType.SPEED)
+  var count = 0
 
   override def getMonitorTypes(param: String) = monitorTypes
 
@@ -25,9 +25,9 @@ object GpsCollector extends DriverOps {
   override def verifyParam(json: String) = {
     implicit val reads = Json.reads[GpsParam]
     val ret = Json.parse(json).validate[GpsParam]
-    ret.fold(err=>{
+    ret.fold(err => {
       throw new IllegalArgumentException(JsError(err).toString)
-    }, param=>
+    }, param =>
       json
     )
   }
@@ -71,6 +71,8 @@ class SerialDataReader(serialComm: SerialComm) extends AbstractDataReader {
     for (line <- serialComm.getLine3())
       lineBuffer.append(line.trim)
 
+    Logger.info(s"gps read ${lineBuffer.toList}")
+
     if (lineBuffer.isEmpty)
       null
     else
@@ -79,7 +81,7 @@ class SerialDataReader(serialComm: SerialComm) extends AbstractDataReader {
 }
 
 class GpsCollector @Inject()(monitorTypeDB: MonitorTypeDB)(@Assisted id: String, @Assisted protocolParam: ProtocolParam,
- @Assisted gpsParam: GpsParam) extends Actor
+                                                           @Assisted gpsParam: GpsParam) extends Actor
   with ActorLogging with SentenceListener with ExceptionListener with PositionListener {
   Logger.info(s"$id $protocolParam")
 
@@ -94,10 +96,12 @@ class GpsCollector @Inject()(monitorTypeDB: MonitorTypeDB)(@Assisted id: String,
   var reader: SentenceReader = _
   var buffer: Option[BufferedReader] = None
   var timer: Option[Cancellable] = None
-
-  def receive = handler(MonitorStatus.NormalStat)
+  @volatile var lastPositionOpt: Option[Position] = None
 
   init
+  @volatile var lastTimeOpt: Option[Long] = None
+
+  def receive = handler(MonitorStatus.NormalStat)
 
   def handler(collectorState: String): Receive = {
     case SetState(id, state) =>
@@ -129,13 +133,40 @@ class GpsCollector @Inject()(monitorTypeDB: MonitorTypeDB)(@Assisted id: String,
       buff.close()
   }
 
-  @volatile var lastPositionOpt : Option[Position] = None
-  @volatile var lastTimeOpt : Option[Long] = None
+  @volatile var counter = 0
+  def providerUpdate(evt: PositionEvent) {
+    val latValue = MonitorTypeData(MonitorType.LAT, evt.getPosition.getLatitude, MonitorStatus.NormalStat)
+    val lngValue = MonitorTypeData(MonitorType.LNG, evt.getPosition.getLongitude, MonitorStatus.NormalStat)
+    val altValue = MonitorTypeData(MonitorType.ALTITUDE, evt.getPosition.getAltitude, MonitorStatus.NormalStat)
+    val now = DateTime.now().getMillis
+    val speedValue = for (lastTime <- lastTimeOpt; distance <- getDistance(evt.getPosition)) yield
+      distance / (now - lastTime)
 
-  private def getDistance(pos:Position): Option[Double] = {
+    //Update time and pos
+    lastTimeOpt = Some(now)
+    lastPositionOpt = Some(evt.getPosition)
+
+    if(counter % 100 == 0)
+      checkWithinRange(evt.getPosition)
+
+    counter = counter + 1
+
+    val fullList = if (speedValue.nonEmpty) {
+      val speed = MonitorTypeData(MonitorType.SPEED, speedValue.get, MonitorStatus.NormalStat)
+      List(latValue, lngValue, altValue, speed)
+    } else
+      List(latValue, lngValue, altValue)
+
+
+    context.parent ! ReportData(fullList)
+  }
+
+  private def getDistance(pos: Position): Option[Double] = {
+    /*
     val R = 6371 // Radius of the earth
-
-    for(lastPos<-lastPositionOpt) yield {
+     */
+/*
+    for (lastPos <- lastPositionOpt) yield {
       val latDistance = Math.toRadians(pos.getLatitude - lastPos.getLatitude)
       val lonDistance = Math.toRadians(pos.getLongitude - lastPos.getLongitude)
       val a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) +
@@ -147,55 +178,24 @@ class GpsCollector @Inject()(monitorTypeDB: MonitorTypeDB)(@Assisted id: String,
       //val distanceWithAlt = Math.pow(distance, 2) + Math.pow(height, 2)
       distance
     }
+    */
+
+    for (lastPos <- lastPositionOpt) yield
+      lastPos.distanceTo(pos)
   }
 
-  private def getFixedDistance(pos:Position, lat:Double, lon:Double) : Double = {
-    val R = 6371 // Radius of the earth
-
-
-      val latDistance = Math.toRadians(pos.getLatitude - lat)
-      val lonDistance = Math.toRadians(pos.getLongitude - lon)
-      val a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) +
-        Math.cos(Math.toRadians(lon)) * Math.cos(Math.toRadians(pos.getLongitude)) * Math.sin(lonDistance / 2) *
-          Math.sin(lonDistance / 2)
-      val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-      //val height = pos.getAltitude - lastPos.getAltitude
-      val distance = R * c * 1000 // convert to meters
-      //val distanceWithAlt = Math.pow(distance, 2) + Math.pow(height, 2)
-      distance
-
-  }
-
-  def checkWithinRange(pos:Position): Unit ={
-    for(lat<-gpsParam.lat;lon<-gpsParam.lon;radius<-gpsParam.radius; enable<-gpsParam.enableAlert if enable){
-      val distance = getFixedDistance(pos, lat, lon)
+  def checkWithinRange(pos: Position): Unit = {
+    for (lat <- gpsParam.lat; lon <- gpsParam.lon; radius <- gpsParam.radius; enable <- gpsParam.enableAlert if enable) {
+      val center = new Position(lat, lon)
+      val distance = center.distanceTo(pos)
       Logger.info(s"Distance from center=$distance")
-      if(distance > radius)
+      if (distance > radius)
         context.parent ! WriteSignal(GPS_OUT_OF_RANGE, true)
     }
   }
-  def providerUpdate(evt: PositionEvent) {
-    val latValue = MonitorTypeData(MonitorType.LAT, evt.getPosition.getLatitude, MonitorStatus.NormalStat)
-    val lngValue = MonitorTypeData(MonitorType.LNG, evt.getPosition.getLongitude, MonitorStatus.NormalStat)
-    val altValue = MonitorTypeData(MonitorType.ALTITUDE, evt.getPosition.getAltitude, MonitorStatus.NormalStat)
-    val now = DateTime.now().getMillis
-    val speedValue = for(lastTime<-lastTimeOpt;distance <- getDistance(evt.getPosition)) yield
-      distance/(now - lastTime)
 
-    //Update time and pos
-    lastTimeOpt = Some(now)
-    lastPositionOpt = Some(evt.getPosition)
-
-    checkWithinRange(evt.getPosition)
-
-    val fullList = if(speedValue.nonEmpty) {
-      val speed = MonitorTypeData(MonitorType.SPEED, speedValue.get, MonitorStatus.NormalStat)
-      List(latValue, lngValue, altValue, speed)
-    }else
-      List(latValue, lngValue, altValue)
-
-
-    context.parent ! ReportData(fullList)
+  private def getFixedDistance(pos: Position, lastPos:Position): Double = {
+    lastPos.distanceTo(pos)
   }
 
   def onException(ex: Exception) {
