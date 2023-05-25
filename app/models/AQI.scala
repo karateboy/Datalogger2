6124a4a6269a16b566dffa2fbab1e3e772022edb
karateboy <karateboy.tw@gmail.com>
2023-05-25 15:58:45 +0800

    Fix AQI O3 precision

--- app/models/AQI.scala
+++ app/models/AQI.scala
@@ -85,14 +85,12 @@ object AQI {
         val (aqiMt, subReport) = pair
         if (aqiMt == O3_8hr || aqiMt == O3)
           AqiSubExplain(getAqiMonitorTypeName(aqiMt), AqiExplain(subReport.aqi.map(_.toInt.toString).getOrElse("-"),
-            monitorTypeDB.format(mtMap(aqiMt), subReport.value.map(_ / 1000)),
+            monitorTypeDB.format(mtMap(aqiMt), subReport.value.map(_ / 1000), Some(3)),
             subReport.aqi.map(getAqiLevel).getOrElse("")))
         else {
           val precision = if (aqiMt == pm10) Some(0)
           else if (aqiMt == pm25)
             Some(1)
-          else if (aqiMt == O3_8hr || aqiMt == O3)
-            Some(3)
           else
             None
 
