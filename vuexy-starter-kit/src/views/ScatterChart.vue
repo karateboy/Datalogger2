8c90383bbec05ff32eeb8a71325dc61b0647335e
黃澤熹 <karateboy@stronghold.tw>
2023-11-14 17:13:36 +0800

    1. port regression charts from central to logger

--- vuexy-starter-kit/src/views/ScatterChart.vue
+++ vuexy-starter-kit/src/views/ScatterChart.vue
@@ -2,85 +2,71 @@
   <div>
     <b-card>
       <b-form @submit.prevent>
+        <b-form-group label="測點" label-for="monitor" label-cols-md="3">
+          <v-select
+            id="monitor"
+            v-model="form.monitors"
+            label="desc"
+            :reduce="mt => mt._id"
+            :options="monitors"
+            :close-on-select="false"
+            multiple
+          />
+        </b-form-group>
+        <b-form-group label="測項" label-for="monitorType" label-cols-md="3">
+          <v-select
+            id="monitorType"
+            v-model="form.monitorTypes"
+            label="desp"
+            :reduce="mt => mt._id"
+            :options="activatedMonitorTypes"
+            :close-on-select="false"
+            multiple
+          />
+        </b-form-group>
+        <b-form-group label="資料種類" label-for="dataType" label-cols-md="3">
+          <v-select
+            id="dataType"
+            v-model="form.dataType"
+            label="txt"
+            :reduce="dt => dt.id"
+            :options="dataTypes"
+          />
+        </b-form-group>
+        <b-form-group
+          label="回歸趨勢模式"
+          label-for="regressionModes"
+          label-cols-md="3"
+        >
+          <v-select
+            id="regressionModes"
+            v-model="form.regressionSettings.type"
+            label="txt"
+            :reduce="dt => dt.id"
+            :options="regressionModes"
+          />
+        </b-form-group>
+        <b-form-group label="狀態" label-for="statusFilter" label-cols-md="3">
+          <v-select
+            id="statusFilter"
+            v-model="form.statusFilter"
+            label="txt"
+            :reduce="dt => dt.id"
+            :options="statusFilters"
+          />
+        </b-form-group>
+        <b-form-group label="資料區間" label-for="dataRange" label-cols-md="3">
+          <date-picker
+            id="dataRange"
+            v-model="form.range"
+            :range="true"
+            type="datetime"
+            format="YYYY-MM-DD HH:mm"
+            value-type="timestamp"
+            :show-second="false"
+          />
+        </b-form-group>
         <b-row>
-          <b-col cols="12">
-            <b-form-group label="測點" label-for="monitor" label-cols-md="3">
-              <v-select
-                id="monitor"
-                v-model="form.monitors"
-                label="desc"
-                :reduce="mt => mt._id"
-                :options="monitors"
-                :close-on-select="false"
-                multiple
-              />
-            </b-form-group>
-          </b-col>
-          <b-col cols="12">
-            <b-form-group
-              label="測項"
-              label-for="monitorType"
-              label-cols-md="3"
-            >
-              <v-select
-                id="monitorType"
-                v-model="form.monitorTypes"
-                label="desp"
-                :reduce="mt => mt._id"
-                :options="activatedMonitorTypes"
-                :close-on-select="false"
-                multiple
-              />
-            </b-form-group>
-          </b-col>
-          <b-col cols="12">
-            <b-form-group
-              label="資料種類"
-              label-for="dataType"
-              label-cols-md="3"
-            >
-              <v-select
-                id="dataType"
-                v-model="form.dataType"
-                label="txt"
-                :reduce="dt => dt.id"
-                :options="dataTypes"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col cols="12">
-            <b-form-group
-              label="狀態"
-              label-for="statusFilter"
-              label-cols-md="3"
-            >
-              <v-select
-                id="statusFilter"
-                v-model="form.statusFilter"
-                label="txt"
-                :reduce="dt => dt.id"
-                :options="statusFilters"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col cols="12">
-            <b-form-group
-              label="資料區間"
-              label-for="dataRange"
-              label-cols-md="3"
-            >
-              <date-picker
-                id="dataRange"
-                v-model="form.range"
-                :range="true"
-                type="datetime"
-                format="YYYY-MM-DD HH:mm"
-                value-type="timestamp"
-                :show-second="false"
-              />
-            </b-form-group>
-          </b-col>
-          <!-- submit and reset -->
           <b-col offset-md="3">
             <b-button
               v-ripple.400="'rgba(255, 255, 255, 0.15)'"
@@ -96,14 +82,15 @@
       </b-form>
     </b-card>
     <b-card v-show="display">
-      <b-card-body><div id="chart_container"></div></b-card-body>
+      <b-card-body>
+        <div id="chart_container"></div>
+      </b-card-body>
     </b-card>
   </div>
 </template>
 <style lang="scss">
 @import '@core/scss/vue/libs/vue-select.scss';
-</style>
-<style>
+
 .highcharts-container,
 .highcharts-container svg {
   width: 100% !important;
@@ -121,6 +108,9 @@ import useAppConfig from '../@core/app-config/useAppConfig';
 import moment from 'moment';
 import axios from 'axios';
 import highcharts from 'highcharts';
+const jStat = require('jstat');
+const highchartsRegression = require('highcharts-regression');
+highchartsRegression(highcharts);
 
 export default Vue.extend({
   components: {
@@ -135,6 +125,10 @@ export default Vue.extend({
       moment().subtract(1, 'days').startOf('day').valueOf(),
       moment().add(1, 'days').startOf('day').valueOf(),
     ];
+    let regressionSettings: any = {
+      type: 'linear',
+      name: '%eq R^2:%r2 標準差:%se',
+    };
     return {
       dataTypes: [
         { txt: '小時資料', id: 'hour' },
@@ -147,40 +141,25 @@ export default Vue.extend({
         { id: 'maintance', txt: '維修' },
         { id: 'invalid', txt: '無效數據' },
         { id: 'valid', txt: '有效數據' },
+        { id: 'validWithoutEngineExhaustion', txt: '有效數據(不含引擎排放)' },
+        { id: 'engineExhaustion', txt: '引擎排放' },
+      ],
+      regressionModes: [
+        { id: 'linear', txt: '線性' },
+        { id: 'exponential', txt: 'exponential指數' },
+        { id: 'polynomial', txt: 'polynomial多項式' },
+        { id: 'power', txt: 'power' },
+        { id: 'logarithmic', txt: 'logarithmic對數式' },
+        { id: 'loess', txt: 'loess' },
       ],
       display: false,
-      chartTypes: [
-        {
-          type: 'line',
-          desc: '折線圖',
-        },
-        {
-          type: 'spline',
-          desc: '曲線圖',
-        },
-        {
-          type: 'area',
-          desc: '面積圖',
-        },
-        {
-          type: 'areaspline',
-          desc: '曲線面積圖',
-        },
-        {
-          type: 'column',
-          desc: '柱狀圖',
-        },
-        {
-          type: 'scatter',
-          desc: '點圖',
-        },
-      ],
       form: {
         monitors: Array<string>(),
         monitorTypes: Array<string>(),
         dataType: 'hour',
         statusFilter: 'all',
         range,
+        regressionSettings,
       },
     };
   },
@@ -226,17 +205,6 @@ export default Vue.extend({
         }/${this.form.range[1]}`;
         const res = await axios.get(url);
         const ret = res.data;
-
-        ret.legend = {
-          layout: 'vertical',
-          align: 'left',
-          verticalAlign: 'top',
-          x: 100,
-          y: 70,
-          floating: true,
-          borderWidth: 1,
-        };
-
         let mt1 = this.mtMap.get(this.form.monitorTypes[0]);
         let mt2 = this.mtMap.get(this.form.monitorTypes[1]);
         ret.plotOptions = {
@@ -259,37 +227,29 @@ export default Vue.extend({
             },
             tooltip: {
               headerFormat: '<b>{series.name}</b><br>',
-              pointFormat: `${mt1.desp}{point.x} ${mt1.unit}, ${mt2.desp}{point.y} ${mt2.unit}`,
+              pointFormat: `${mt1.desp}:{point.x} ${mt1.unit}, ${mt2.desp}:{point.y} ${mt2.unit}`,
             },
           },
         };
 
-        ret.colors = [
-          '#7CB5EC',
-          '#434348',
-          '#90ED7D',
-          '#F7A35C',
-          '#8085E9',
-          '#F15C80',
-          '#E4D354',
-          '#2B908F',
-          '#FB9FA8',
-          '#91E8E1',
-          '#7CB5EC',
-          '#80C535',
-          '#969696',
-        ];
-
         let prec = Math.max(mt1.prec, mt2.prec);
 
         ret.tooltip = { valueDecimals: prec };
-        //ret.legend = { enabled: true };
         ret.credits = {
           enabled: false,
           href: 'http://www.wecc.com.tw/',
         };
-
-        highcharts.chart('chart_container', ret);
+        let series = ret.series as Array<any>;
+        if (series[0].data.length !== 0) {
+          let combinedIndex = series.length - 1;
+          series[combinedIndex].regression = true;
+          series[combinedIndex].visible = false;
+          series[combinedIndex].regressionSettings = {
+            type: this.form.regressionSettings.type,
+            name: '%eq R^2:%r2',
+          };
+          highcharts.chart('chart_container', ret);
+        } else this.$bvModal.msgBoxOk('沒有足夠資料');
       } catch (err) {
         throw Error(`${err}`);
       } finally {
