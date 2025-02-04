dec8c723cd351e9770edde8eb15ada1bff736a40
karateboy <karateboy.tw@gmail.com>
2025-02-04 17:56:32 +0800

    Fix Opendata URL change

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -763,8 +763,7 @@ class HomeController @Inject()(environment: play.api.Environment,
   }
 
   def weatherReport(): Action[AnyContent] = Security.Authenticated.async {
-    val f = WSClient.url(s"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-033?format=JSON&locationName=萬巒鄉")
-      .withHeaders(("Authorization", "CWB-978789A6-C800-47D7-B4C6-5BF330B61FA6"))
+    val f = WSClient.url(s"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-033?Authorization=CWB-978789A6-C800-47D7-B4C6-5BF330B61FA6&limit=1&LocationName=萬巒鄉")
       .get()
     for (ret <- f) yield {
       implicit val r7 = Json.reads[ElementValue]
