38a5ba0c4aa6a26b34868316346e43278419ba0b
karateboy <karateboy.tw@gmail.com>
2025-07-07 11:49:48 +0800

    1. Add datetime field in upload

--- app/models/NewTaipeiOpenData.scala
+++ app/models/NewTaipeiOpenData.scala
@@ -5,6 +5,8 @@ import play.api.libs.json.{Json, OWrites}
 import play.api.libs.ws.WSClient
 import play.api.{Configuration, Logger}
 
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
@@ -16,7 +18,13 @@ class NewTaipeiOpenData @Inject()(WSClient: WSClient,
                                   alarmDB: AlarmDB,
                                   monitorTypeDB: MonitorTypeDB,
                                   configuration: Configuration) extends Enumeration {
-  private case class DataEntry(SiteId: String, ItemId: String, ItemEngName: String, ItemChName: String, ItemValue: Option[Double], ItemUnit: String)
+  private case class DataEntry(SiteId: String,
+                               ItemId: String,
+                               ItemEngName: String,
+                               ItemChName: String,
+                               ItemValue: Option[Double],
+                               ItemUnit: String,
+                               DateTime: String)
 
   private case class PayLoad(pid: String, content: Seq[DataEntry])
 
@@ -33,30 +41,36 @@ class NewTaipeiOpenData @Inject()(WSClient: WSClient,
 
   val logger: Logger = Logger(getClass)
 
-  def upload(recordList: RecordList, mtConfigs: Seq[CdxMonitorType]): Unit =
+  def upload(recordList: RecordList, mtConfigs: Seq[CdxMonitorType], dryRun: Boolean = false): Unit =
     for (config <- getConfig if config.enable) {
 
       try {
-        val dataEntries = recordList.mtDataList.zipWithIndex flatMap  { pair =>
+        val localDateTime = recordList._id.time.toInstant
+          .atZone(ZoneId.systemDefault())
+          .toLocalDateTime
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss")
+
+        val dateTimeString = localDateTime.format(formatter)
+        val dataEntries = recordList.mtDataList.zipWithIndex flatMap { pair =>
           val (mtRecord, idx) = pair
           val mt = monitorTypeDB.map(mtRecord.mtName)
-          val cdxMtConfig =  mtConfigs.find(mtConfig=>mtConfig.mt == mtRecord.mtName).getOrElse(CdxMonitorType(mtRecord.mtName, mtRecord.mtName, None, None))
+          val cdxMtConfig = mtConfigs.find(mtConfig => mtConfig.mt == mtRecord.mtName).getOrElse(CdxMonitorType(mtRecord.mtName, mtRecord.mtName, None, None))
 
           cdxMtConfig match {
-            case CdxMonitorType(_, _, Some(min), Some(max))=>
-              for(v<-mtRecord.value if v>= min && v <= max) yield
-                DataEntry(config.pid, "%02d".format(idx + 1), mt._id, mt.desp, mtRecord.value, mt.unit)
+            case CdxMonitorType(_, _, Some(min), Some(max)) =>
+              for (v <- mtRecord.value if v >= min && v <= max) yield
+                DataEntry(config.pid, "%02d".format(idx + 1), mt._id, mt.desp, mtRecord.value, mt.unit, dateTimeString)
 
-            case CdxMonitorType(_, _, None, Some(max))=>
-              for(v<-mtRecord.value if v <= max) yield
-                DataEntry(config.pid, "%02d".format(idx + 1), mt._id, mt.desp, mtRecord.value, mt.unit)
+            case CdxMonitorType(_, _, None, Some(max)) =>
+              for (v <- mtRecord.value if v <= max) yield
+                DataEntry(config.pid, "%02d".format(idx + 1), mt._id, mt.desp, mtRecord.value, mt.unit, dateTimeString)
 
-            case CdxMonitorType(_, _, Some(min), None)=>
-              for(v<-mtRecord.value if v>= min) yield
-                DataEntry(config.pid, "%02d".format(idx + 1), mt._id, mt.desp, mtRecord.value, mt.unit)
+            case CdxMonitorType(_, _, Some(min), None) =>
+              for (v <- mtRecord.value if v >= min) yield
+                DataEntry(config.pid, "%02d".format(idx + 1), mt._id, mt.desp, mtRecord.value, mt.unit, dateTimeString)
 
-            case _=>
-              Some(DataEntry(config.pid, "%02d".format(idx + 1), mt._id, mt.desp, mtRecord.value, mt.unit))
+            case _ =>
+              Some(DataEntry(config.pid, "%02d".format(idx + 1), mt._id, mt.desp, mtRecord.value, mt.unit, dateTimeString))
           }
         }
         val payload = PayLoad(config.pid, dataEntries)
@@ -64,6 +78,12 @@ class NewTaipeiOpenData @Inject()(WSClient: WSClient,
         implicit val w1: OWrites[DataEntry] = Json.writes[DataEntry]
         implicit val w2: OWrites[PayLoad] = Json.writes[PayLoad]
         val postUrl = s"${config.url}api/v1/open.dataset.content.update"
+
+        if(dryRun) {
+          logger.info(s"Dry run: would post to $postUrl with payload: ${Json.toJson(payload)}")
+          return
+        }
+
         val f = WSClient.url(postUrl)
           .addHttpHeaders("Content-Type" -> "application/json", "Authorization" -> config.api)
           .post(Json.toJson(payload))
