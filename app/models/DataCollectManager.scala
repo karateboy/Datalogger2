b150eed283f0ab9930a404c6f286a97359f17d0e
karateboy <karateboy.tw@gmail.com>
2025-08-27 00:29:54 +0800

    1. for E1212 Rain, no reset counter. 2. for accumulate RAIN, calculate diff for every min and sum for hour rain.

--- app/models/DataCollectManager.scala
+++ app/models/DataCollectManager.scala
@@ -263,9 +263,15 @@ object DataCollectManager {
                 directionAvg(windSpeed, values)
               }
             case MonitorType.RAIN =>
-              if (mtCase.accumulated.contains(true))
-                Some(values.max)
-              else
+              if (mtCase.accumulated.contains(true)) {
+                val sumOfDiff = values.sliding(2).map(pair =>
+                  if (pair.last >= pair.head)
+                    pair.last - pair.head
+                  else
+                    0d
+                  ).sum
+                Some(sumOfDiff)
+              } else
                 Some(values.sum)
             case MonitorType.PM10 =>
               if (LoggerConfig.config.pm25HourAvgUseLastRecord)
